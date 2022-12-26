68c435cde9d3b481be460d526792e076df5f0214
mahmoudAhmed1919 <121295627+mahmoudAhmed1919@users.noreply.github.com>
2022-12-26 05:57:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers to get the perfect numbers between them : ");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = n1; i <= n2; ++i) {
+                int cnt = 1;
+                for (int j = i / 2; j >= 2; j--) {
+                    if (i % j == 0) cnt += j;
+                }
+                if (cnt == i) Console.WriteLine(i);
+            }
+        }
+    }
+}
